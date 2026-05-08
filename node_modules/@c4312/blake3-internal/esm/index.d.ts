c05f7fc90a21849b17dfba2d4979212d86e021c8
mubariz <mubarizharoonmulpk@gmail.com>
2026-05-08 12:08:51 +0500

    Stop tracking files now ignored via .gitignore

--- node_modules/@c4312/blake3-internal/esm/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './hash-fn.js';
-export * from './hash-instance.js';
-export * from './hash-oneshots.js';
-export * from './hash-reader.js';
-export * from './wasm-types.js';
--- node_modules/@c4312/blake3-native/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from '@c4312/blake3-internal';
-export * from './hash-fn';
-export * from './hash-instance';
-
-export const load = () => Promise.resolve();
